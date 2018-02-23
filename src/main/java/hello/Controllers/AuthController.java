5f974dbe8d19654608f870df73ab2ca4939f6c40
Danilo Henrique de Melo Martins Andrade <vectorx_dhmartins@polimed.dom>
2018-02-23 11:25:24 -0300

    Fix: SSL certificate error and Riot Service adjusted. Need separate the projects.

--- src/main/java/hello/Controllers/AuthController.java
+++ src/main/java/hello/Controllers/AuthController.java
@@ -1,6 +1,7 @@
 package hello.Controllers;
 
 import hello.JsonReader;
+import hello.SSLFixer;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
@@ -15,6 +16,11 @@ public class AuthController extends JsonReader {
 
     @RequestMapping("/auth")
     public String login(@RequestParam(value="code", required=true) String authCode, Model model){
+
+        // Alternative way to pass the bypass ssl certification without verify
+        SSLFixer ssl = new SSLFixer();
+        ssl.TrustSSLCertificates();
+
         model.addAttribute("authCode", authCode);
 
         String urlAccessToken = "https://api.instagram.com/oauth/access_token?client_id=68bcc1d330a847f99c46938021d4df40&client_secret=d4bd8c139123435cb04a275b31a2fed1&grant_type=authorization_code&redirect_uri=http://localhost:8080/auth&code=" + authCode;
@@ -33,5 +39,61 @@ public class AuthController extends JsonReader {
 
         return "home";
     }
+
+
+    /*
+    public String accessTkn (String code) {
+
+        try {
+
+            HttpClient httpclient = HttpClients.createDefault();
+            HttpPost httppost = new HttpPost("https://api.instagram.com/oauth/access_token");
+
+            // Request parameters and other properties.
+            List<NameValuePair> params = new ArrayList<NameValuePair>(2);
+            params.add(new BasicNameValuePair("client_id", Constants.CLIENT_ID));
+            params.add(new BasicNameValuePair("client_secret", Constants.CLIENT_SECRET));
+            params.add(new BasicNameValuePair("grant_type", "authorization_code"));
+            params.add(new BasicNameValuePair("redirect_uri",  Constants.REDIRECT_URI_AUTH));
+            params.add(new BasicNameValuePair("code",  code));
+
+
+            httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+
+            //Execute and get the response.
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            System.out.println("entity "+ entity.getContent());
+
+            if (entity != null) {
+                InputStream instream = entity.getContent();
+                try {
+
+                    return (getStringfromStream(instream));
+                    // do something useful
+                } finally {
+                    instream.close();
+                }
+            }
+        } catch (UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (UnsupportedOperationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return "Abhay";
+
+
+    }
+    */
+
 
 }
--- src/main/java/hello/Controllers/SummonerController.java
+++ src/main/java/hello/Controllers/SummonerController.java
@@ -1,6 +1,7 @@
 package hello.Controllers;
 
 import hello.JsonReader;
+import hello.SSLFixer;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,9 +16,13 @@ public class SummonerController extends JsonReader{
 
         JSONObject summoner = null;
 
-        String url = "https://" + location + "1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + summonerName + "?api_key=RGAPI-9ec97eb6-1968-458e-8e79-1400102dbf9f";
+        String apiKey = "RGAPI-e3645fd6-cf23-43d7-8872-c1f676111d43";
+        String url = "https://" + location + "1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + summonerName + "?api_key=" + apiKey;
+        System.out.println("Acessando: " + url);
 
         try {
+            SSLFixer ssl = new SSLFixer();
+            ssl.TrustSSLCertificates();
             summoner = getJson(url);
         } catch (Exception e){
             System.out.println("Ocorreu um erro durante a obtencao do JSON. \n" + url + "\n" + e);
